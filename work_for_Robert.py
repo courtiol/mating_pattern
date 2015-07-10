6d371c2d0fe28329526fd4e744c9c1ad13770030
RSchwieger <Roberttton@gmail.com>
2015-07-10 16:52:13 +0200

    debug

--- work_for_Robert.py
+++ work_for_Robert.py
@@ -1,7 +1,8 @@
 import numpy as np
 from itertools import product
-import math
 from likelihood_n_by_m import compute_h, computeA
+from math import factorial
+
 
 def computeGeneralRecurrence1(Q, P):
     """
@@ -20,22 +21,41 @@ def computeGeneralRecurrence1(Q, P):
 
     # Create an array of specified dimension
     grid = [range(i+1) for i in shape]
-    zeroCoordinate = tuple( (0 for x in range(sizeShape)) )
+    zeroCoordinate = tuple( (0 for _ in range(sizeShape)) )
     A[zeroCoordinate] = 1
 
     for coordinate in product(*grid): # iterate over A
         if coordinate != zeroCoordinate:
-           print("------------------------------------")
-           A[coordinate] = computeA(coordinate, A) # type: ?
            h = compute_h(coordinate,nrows, ncols,  P) # type: ?
-           A[coordinate] *= 1/h
+           A[coordinate] = computeA(coordinate, A)/h # type: ?
 
 
     return A[tuple(shape)]
 
+
+def computeGeneralPMatingpattern1(Q, P):
+    unity_x = [1 for i in range(Q.ncols())] # ToDo: change to np.shape()
+    unity_y = [1 for i in range(Q.nrows())]
+    x = np.dot(np.array(Q),unity_x)
+    y = np.dot(np.transpose(np.array(Q)), unity_y)
+
+    prod = 1
+    for xi in x:
+        prod = prod*factorial(xi)
+    for yi in y:
+        prod = prod*factorial(yi)
+
+    for i in range(Q.nrows()): # Or use np.flatten
+        for j in range(Q.ncols()):
+            prod = prod*(P[i][j]**Q[i][j] )
+    print("prod "+str(prod))
+    print("cP "+str(computeGeneralRecurrence1(Q, P)))
+
+    return prod*computeGeneralRecurrence1(Q, P)
+
 # Test the function
 P = np.array([[1.0,1.0] ,[1.0,1.0]], dtype=float)
-Q = np.array([[1,2] ,[3,4]], dtype=int)
+Q = np.array([[2,1] ,[1,2]], dtype=int)
 
 result = computeGeneralRecurrence1(Q, P)
 
