f2d6297d7d5e938c6ee8be7edb0d44c30ebffe07
courtiol <alexandre.courtiol@gmail.com>
2015-07-10 19:08:52 +0200

    all files merge into one!

--- likelihood_n_by_m.py
+++ likelihood_n_by_m.py
@@ -1,72 +1,134 @@
 import numpy as np
 from itertools import product
-import math
+from math import factorial
+
+
+def recover_types(Q):
+    # input:
+    #  - Q: np.array of a mating pattern
+    # output:
+    #  - x, y: vectors of males and females
+    unity_x = [1 for i in range(np.shape(Q)[0])]
+    unity_y = [1 for i in range(np.shape(Q)[1])]
+    x = np.dot(Q, unity_x)
+    y = np.dot(np.transpose(Q), unity_y)
+    return x, y
+
+'''
+# example
+Q_test = np.array([[1, 2], [5, 2]])
+print(recover_types(Q_test))
+'''
+
 
 def computeA(coordinate, A):
+    # input:
+    #  - coordinate: a tuple, representing a given mating pattern
+    #  - A: a multidimensional np.array
+    # output:
+    #  - result: an int, next term of the recurrence
+
     result = 0
     for i in range(len(coordinate)):
         if coordinate[i] > 0:
-            # create coordinate coordinate-e_i
-            tempCoordinate = np.copy(coordinate)
-            tempCoordinate[i] = tempCoordinate[i]-1
-            tempCoordinate = tuple(tempCoordinate)
-            result = result + A[tempCoordinate]        
+            # new tuple with same coordinates but -1 at position i:
+            temp_coordinate = coordinate[:i]+(coordinate[i]-1,)+coordinate[(i+1):]
+            result += A[temp_coordinate]
     return result
-    
-def compute_h(coordinate, n, m,  P):
-    C = np.reshape(coordinate, (n, m))
-    unity_x = [1 for i in range(m)]
-    unity_y = [1 for i in range(n)]
-    x = np.dot(C,unity_x)
-    y = np.dot(np.transpose(C), unity_y)
-    return np.dot(y,np.dot(P,x))
-    
+'''
+# example in 2 dimensions:
+A_test = np.array([[0, 2, 0], [5, 0, 0], [0, 0, 0]])
+print(A_test)
+coordinate_test = (1, 1)
+print(computeA(coordinate_test, A_test)) # should be 7 (= 2 + 5)
+'''
+
+
+def compute_h(coordinate, n, m, P):
+    # input:
+    #  - coordinate: a tuple
+    #  - n: an integer defining the number of male types
+    #  - m: an integer defining the number of female types
+    #  - P: the preference matrix (np.array)
+    # output:
+    #  - h, the numerator of the probability that any mating occur during one time step
+
+    mating_pattern = np.reshape(np.array(coordinate), (n, m))
+    x, y = recover_types(mating_pattern)
+    h = np.dot(y, np.dot(P, x))
+    return h
+
+'''
+# example:
+coordinate_test = (1, 2, 1, 1)
+P_test = np.array([[0.2, 0.1], [0.4, 1.0]])
+print(P_test)
+print(compute_h(coordinate_test, 2, 2, P_test))
+'''
+
+
+def computeRecurrenceFactor(Q, P):
+    # input:
+    #  - Q: np.array of a mating pattern
+    #  - P: np.array of a mating preferences
+    # output:
+    #  - prod: the product needed for recurrence computation
+    x, y = recover_types(Q)
+    prod = np.prod([factorial(i) for i in np.concatenate((x, y))])
+    prod *= np.prod(P.flatten()**Q.flatten())
+    return prod
+
+'''
+# example:
+Q_test = np.array([[1, 2], [5, 2]])
+P_test = np.array([[0.2, 0.1], [0.4, 1.0]])
+print(computeRecurrenceFactor(Q_test, P_test))
+'''
+
+
 def computeGeneralRecurrence1(Q, P):
-    n = Q[0][0]
-    sizeShape = Q.ncols() * Q.nrows()
+    """
+    This method computes the recurrence A[Q] = 1/h* ( A[Q-E11] + ... A[Q-E_mn])
+    :param Q: multidimensional numpy.array of type <class 'numpy.ndarray'> with coefficients of <class 'numpy.int64'>
+    :param P: multidimensional numpy.array of type <class 'numpy.ndarray'> with coefficients of <class 'numpy.float64'>
+    :return: the recurrence term A[Q] of type <class 'numpy.float64'>
+    """
+    nrows = np.shape(Q)[0]
+    ncols = np.shape(Q)[1]
+    sizeShape = ncols * nrows
+
     shape = (np.reshape(Q, (1, sizeShape))[0]).tolist()
-    
+
+    A = np.ndarray(shape=np.add(shape,1), dtype=float)
+
     # Create an array of specified dimension
-    dimension = Q.nrows()*Q.ncols() # We need an array of this size in each computation step
-    zeroCoordinate = tuple([0 for x in range(dimension)])
     grid = [range(i+1) for i in shape]
-    
-    A = np.ndarray(shape=np.add(shape,1), dtype=int)    
+    zeroCoordinate = tuple( (0 for _ in range(sizeShape)) )
     A[zeroCoordinate] = 1
-    
+
     for coordinate in product(*grid): # iterate over A
         if coordinate != zeroCoordinate:
-           print("------------------------------------")
-           A[coordinate] = computeA(coordinate, A)
-           
-           print("1: A["+str(coordinate)+"] "+str(A[coordinate]))
-           print("1.5: A["+str(coordinate)+"] "+str(float(A[coordinate])))
-           
-           h = compute_h(coordinate,Q.nrows(), Q.ncols(),  P)
-           A[coordinate] = float(1)/float(h)*float(A[coordinate]) # remove float in python
-           
-           print("1/h["+str(coordinate)+"] "+str( float(1)/float(h) ) )
-           print("2: A["+str(coordinate)+"] "+str(A[coordinate]))
-    
+           h = compute_h(coordinate,nrows, ncols,  P) # type: ?
+           A[coordinate] = computeA(coordinate, A)/h # type: ?
+
+
     return A[tuple(shape)]
-           
+
+
 def computeGeneralPMatingpattern1(Q, P):
-    unity_x = [1 for i in range(Q.ncols())] # ToDo: change to np.shape()
-    unity_y = [1 for i in range(Q.nrows())]
-    x = np.dot(np.array(Q),unity_x)
-    y = np.dot(np.transpose(np.array(Q)), unity_y)
-    
-    prod = 1
-    for xi in x:
-        prod = prod*math.factorial(xi)
-    for yi in y:
-        prod = prod*math.factorial(yi)
-
-    for i in range(Q.nrows()): # Or use np.flatten
-        for j in range(Q.ncols()):
-            prod = prod*(P[i][j]**Q[i][j] )
-    print("prod "+str(prod))
-    print("cP "+str(computeGeneralRecurrence1(Q, P)))
-
+    """
+    This method computes the recurrence A[Q] =  f_11*A[Q-E11] + ... f_mn*A[Q-E_mn])
+    :param Q: multidimensional numpy.array of type <class 'numpy.ndarray'> with coefficients of <class 'numpy.int64'>
+    :param P: multidimensional numpy.array of type <class 'numpy.ndarray'> with coefficients of <class 'numpy.float64'>
+    :return: the recurrence term A[Q] of type <class 'numpy.float64'>
+    """
+    prod = computeRecurrenceFactor(Q, P)
     return prod*computeGeneralRecurrence1(Q, P)
+
+
+# Testcase:
+P = np.array([[0.5,1.0] ,[0.7,0.1]], dtype=float)
+Q = np.array([[1,2] ,[2,1]], dtype=int)
+result = computeGeneralPMatingpattern1(Q, P)
+print(result)
 
