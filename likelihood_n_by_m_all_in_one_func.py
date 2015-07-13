1516b03c6bca91b0e9a5b9b18951f511b08a2a83
alex <alexandre.courtiol@gmail.com>
2015-07-13 18:04:38 +0200

    polish

--- likelihood_n_by_m_all_in_one_func.py
+++ likelihood_n_by_m_all_in_one_func.py
@@ -5,15 +5,17 @@ from math import factorial
 
 def likelihood(Q, P):
     shape = Q.flatten()
+    shape_range = range(len(shape))
+    q_shape = np.shape(Q)
     A = np.ones(shape=np.add(shape, 1), dtype=float)  # create nd array initialised at one
     grid = [range(i+1) for i in shape]  # Create an array of specified dimension
     for coordinate in product(*grid):  # iterate over A
         if sum(coordinate) > 0:
             result = 0
-            for i in range(len(coordinate)):
+            for i in shape_range:
                 if coordinate[i] > 0:
                     result += A[coordinate[:i]+(coordinate[i]-1,)+coordinate[(i+1):]] # A[new tuple with same coordinates but -1 at position i] :
-            mating_pattern = np.reshape(np.array(coordinate), np.shape(Q))
+            mating_pattern = np.reshape(np.array(coordinate), q_shape)
             x, y = (mating_pattern.sum(axis=0), mating_pattern.sum(axis=1))
             A[coordinate] = result/np.dot(y, np.dot(P, x))  # result is divided by h
     recurrenceFactor = np.prod([factorial(i) for i in np.concatenate((x, y))])*np.prod(P**Q)  # NB: x and y are same as in Q
@@ -21,12 +23,16 @@ def likelihood(Q, P):
 
 
 if __name__ == '__main__':
-    import yappi
-    yappi.start()
+    #import yappi
+    #yappi.start()
+    import statprof
+    statprof.start()
     try:
         P = np.array([[0.1, 0.001], [0.2, 0.001]], dtype=float)
         Q = np.array([[10, 20], [20, 10]], dtype=int)
         print(likelihood(Q, P))
 
     finally:
-        yappi.get_func_stats().print_all()
+        #yappi.get_func_stats().print_all()
+        statprof.stop()
+        statprof.display()
