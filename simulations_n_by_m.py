58c368b00cef9c5464ecb0198e1169c35973dd09
courtiol <alexandre.courtiol@gmail.com>
2015-07-23 14:20:28 +0200

    new function choose_random that is much faster

--- simulations_n_by_m.py
+++ simulations_n_by_m.py
@@ -2,30 +2,33 @@ import numpy as np
 import random
 
 
-def choose_random(v):
-    v_pct = [i/sum(v) for i in v]
-    choice = np.random.multinomial(1, v_pct)
-    return sum(choice*range(0, len(v)))
+def choose_random(v):  # Attempt to design a faster function (using stochastic acceptance)
+    max_v = max(v)
+    while 1:
+        i = int(random.random()*len(v))
+        if random.random() <= (v[i]/max_v):
+            return i
 
 '''
 # test:
 foo = np.array([0, 10, 20, 10, 60])
-rec = np.array([choose_random(foo) for i in range(0, 10000)])
+rec = np.array([choose_random(foo) for i in range(0, 100000)])
 print([np.mean(rec == i) for i in range(0, len(foo))])
+print("goal = "+str(foo/sum(foo)))
 '''
+
 
 
 def computeMatingPattern(x, y, P):
     if len(x) != len(P[:, 0]) | len(y) != len(P[0, :]):
         print("error: wrong dimensionality of P (or Q)")
         return 0
-    if sum(x) > sum(y):  # ToDo: replace with cases!
+    if sum(x) > sum(y):
         Q = np.zeros((len(x), len(y)+1), dtype="int32")
+    elif sum(x) < sum(y):
+        Q = np.zeros((len(x)+1, len(y)), dtype="int32")
     else:
-        if sum(x) < sum(y):
-            Q = np.zeros((len(x)+1, len(y)), dtype="int32")
-        else:
-            Q = np.zeros((len(x), len(y)), dtype="int32")
+        Q = np.zeros((len(x), len(y)), dtype="int32")
     while sum(x) > 0 and sum(y) > 0:
         cx = choose_random(x)
         cy = choose_random(y)
@@ -84,17 +87,15 @@ def freqMatingPattern(Q, P, number_simu):
 
 
 if __name__ == '__main__':
-    # import yappi
+    import yappi
     # yappi.start()
-    # import statprof
-    # statprof.start()
-    # try:
-    P = np.array([[1.0, 1.0, 0.001], [1.0, 1.0, 0.0001], [0.001, 0.001, 0]], dtype=float)
-    Q = np.array([[10, 10, 0], [10, 10, 10], [0, 0, 0]], dtype=int)
-    print(freqMatingPattern(Q, P, 100))
-'''
+    import statprof
+    statprof.start()
+    try:
+        P = np.array([[1.0, 1.0, 0.001], [1.0, 1.0, 0.0001], [0.001, 0.001, 0]], dtype=float)
+        Q = np.array([[10, 10, 0], [10, 10, 10], [0, 0, 0]], dtype=int)
+        print(freqMatingPattern(Q, P, 100))
     finally:
-        yappi.get_func_stats().print_all()
-        # statprof.stop()
-        # statprof.display()
-'''
+        # yappi.get_func_stats().print_all()
+        statprof.stop()
+        statprof.display()
