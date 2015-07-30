e07f999dad2cb8404460e4e124d53682df65f95b
courtiol <alexandre.courtiol@gmail.com>
2015-07-30 16:25:29 +0200

    updates

--- likelihood_with_Fij.py
+++ likelihood_with_Fij.py
@@ -13,17 +13,15 @@ def likelihood(Q, P):
         if sum(coordinate) > 0:
             result = 0
             mating_pattern = np.reshape(np.array(coordinate), q_shape)
-            x, y = (mating_pattern.sum(axis=0), mating_pattern.sum(axis=1))
-            for i in shape_range:
-                if coordinate[i] > 0:
-                    result += A[coordinate[:i]+(coordinate[i]-1,)+coordinate[(i+1):]] # A[new tuple with same coordinates but -1 at position i] :
-
+            x, y = (mating_pattern.sum(axis=1), mating_pattern.sum(axis=0))
+            for i in range(len(x)):
+                for j in range(len(y)):
+                    index = len(y)*i+j
+                    if coordinate[index] > 0:
+                        result += P[i, j]*x[i]*y[j]*A[coordinate[:index]+(coordinate[index]-1,)+coordinate[(index+1):]] # A[new tuple with same coordinates but -1 at position i] :
             A[coordinate] = result/np.dot(y, np.dot(P, x))  # result is divided by h
-    #recurrenceFactor = np.prod([factorial(i) for i in np.concatenate((x, y))])*np.prod(P**Q)  # NB: x and y are same as in Q
-    #return recurrenceFactor*A[tuple(shape)]
-    log_recurrenceFactor = np.sum([lgamma(i+1) for i in np.concatenate((x, y))])+log(np.prod(P**Q)) # I do lgamma(i+1) as an equivalent of log(factorial(i))
     print("A = " +str(A[tuple(shape)]))
-    return exp(log_recurrenceFactor+log(A[tuple(shape)]))
+    return A[tuple(shape)]
 
 if __name__ == '__main__':
     #import yappi
@@ -39,7 +37,7 @@ if __name__ == '__main__':
     #P = np.array([[1.0, 1.0, 0.01], [1.0, 1.0, 0.01], [0.001, 0.001, 0]], dtype=float)
     #Q = np.array([[10, 10, 0], [10, 10, 0], [0, 0, 0]], dtype=int)
     P = np.array([[0.5, 0.6, 0.8], [0.7, 0.8, 0.9], [0.5, 0.4, 0.2]], dtype=float)
-    Q = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=int)
+    Q = np.array([[20, 10, 10], [10, 20, 10], [1, 1, 1]], dtype=int)
 
 
     print(likelihood(Q, P))
