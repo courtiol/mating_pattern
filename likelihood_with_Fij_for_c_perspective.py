a779c0bcc5b84e53f7109e48667cb9becc93ab2c
alex <alexandre.courtiol@gmail.com>
2015-08-03 14:36:20 +0200

    likelihood without numpy!!

--- likelihood_with_Fij_for_c_perspective.py
+++ likelihood_with_Fij_for_c_perspective.py
@@ -1,19 +1,14 @@
-import numpy as np
-from itertools import product
-from math import factorial, log, exp, lgamma
-
-
 def index_to_coordinate(index, dim):
-    i = np.ones(shape=len(dim), dtype=int)
+    i = [1, ]*len(dim)
     for x in range(0, len(dim)):
-        i[x] = index % (dim[x]+1)
+        i[x] = int(index % (dim[x]+1))
         index -= i[x]
         index /= (dim[x]+1)
     return i
 '''
 # Test:
 for i in range(0, 24):
-    print(str(i)+" "+str(index_to_coordinate(i, dim=np.array([1,2,3], dtype=int))))
+    print(str(i)+" "+str(index_to_coordinate(i, dim=[1,2,3])))
 '''
 
 
@@ -30,13 +25,13 @@ def coordinate_to_index(coordinate, max_coordinate):
 
 '''
 # Test:
-coordinate_test = index_to_coordinate(25, dim=np.array([2,5,2,3], dtype=int))
-coordinate_to_index(coordinate_test, max_coordinate=np.array([2,5,2,3], dtype=int))
+coordinate_test = index_to_coordinate(25, dim=[2,5,2,3])
+coordinate_to_index(coordinate_test, max_coordinate=[2,5,2,3])
 '''
 
 def xy_from_matrix(m, nrow, ncol):
-    x = np.zeros(shape=nrow, dtype=int)
-    y = np.zeros(shape=ncol, dtype=int)
+    x = [0, ]*nrow
+    y = [0, ]*ncol
     for j in range(0, len(x)):
             for i in range(0, len(y)):
                 y[i] += m[len(y)*j+i]
@@ -44,34 +39,40 @@ def xy_from_matrix(m, nrow, ncol):
     return(x, y)
 
 # Test:
-#xy_from_matrix(np.array([1,2,3,5,6,7], dtype=int), 2, 3)
+#xy_from_matrix([1,2,3,5,6,7], 2, 3)
 
 
-def likelihood(Q, P, limit_zero=False):
-    shape = Q.flatten()
-    q_shape = np.shape(Q)
-    A = np.ones(shape=np.add(shape, 1), dtype=float)  # create nd array initialised at one
+def likelihood(Q, P, nrow, ncol, limit_zero=False):
     coordinate_indexes = 1
-    for i in range(len(shape)):
-        coordinate_indexes *= (shape[i]+1)
+    for i in range(len(Q)):
+        coordinate_indexes *= (Q[i]+1)
+    A = [1, ]*coordinate_indexes
     for coordinate_index in range(0, coordinate_indexes):  # iterate over A
-        coordinate = tuple(index_to_coordinate(coordinate_index, shape))
+        coordinate = index_to_coordinate(coordinate_index, Q)
         if sum(coordinate) > 0:
             result = 0
-            x, y = xy_from_matrix(np.array(coordinate, dtype=int), q_shape[0], q_shape[1])
+            x, y = xy_from_matrix(coordinate, nrow, ncol)
             for i in range(len(x)):
                 for j in range(len(y)):
                     index = len(y)*i+j
                     if coordinate[index] > 0:
-                        result += P[i, j]*x[i]*y[j]*A[coordinate[:index]+(coordinate[index]-1,)+coordinate[(index+1):]] # A[new tuple with same coordinates but -1 at position i] :
-            if np.dot(x, np.dot(P, y)) == 0 and result == 0:
+                        pos = coordinate.copy()
+                        pos[index] -= 1
+                        new_index = coordinate_to_index(pos, Q)
+                        tmp = P[i*nrow+j]*x[i]*y[j]*A[new_index]
+                        result += tmp
+            h = 0
+            for i in range(0, nrow):
+                for j in range(0, ncol):
+                    h += P[i*nrow+j]*x[i]*y[j]
+            if h == 0 and result == 0:
                 if limit_zero: # to compute likelihood when pref tend to zero (to consider virtual types)
-                    A[coordinate] = 1
+                    A[coordinate_to_index(coordinate, Q)] = 1
                 else:
-                    A[coordinate] = 0 # to compute likelihood when pref really are zero
+                    A[coordinate_to_index(coordinate, Q)] = 0 # to compute likelihood when pref really are zero
             else:
-                A[coordinate] = result/np.dot(x, np.dot(P, y))  # result is divided by h
-    return A[tuple(shape)]
+                A[coordinate_to_index(coordinate, Q)] = result/h
+    return A[coordinate_to_index(Q, Q)]
 
 if __name__ == '__main__':
     #import yappi
@@ -82,8 +83,10 @@ if __name__ == '__main__':
 
     import time
     start = time.time()
-    P = np.array([[1.0, 0.8], [0.5, 0.2]], dtype=float)
-    Q = np.array([[50, 4], [30, 2]], dtype=int)
+    #P = np.array([[1.0, 0.8], [0.5, 0.2]], dtype=float)
+    #Q = np.array([[50, 4], [30, 2]], dtype=int)
+    P = [1.0, 0.8, 0.5, 0.2]
+    Q = [50, 4, 30, 2]
     #P = np.array([[0.0, 0.0], [0.0, 0.0]], dtype=float)
     #Q = np.array([[1, 1], [1, 1]], dtype=int)
     #P = np.array([[1.0, 1.0, 0.01], [1.0, 1.0, 0.01], [0.001, 0.001, 0]], dtype=float)
@@ -92,7 +95,7 @@ if __name__ == '__main__':
     #Q = np.array([[20, 10, 10], [10, 20, 10], [1, 1, 1]], dtype=int)
 
 
-    print(likelihood(Q, P))
+    print(likelihood(Q, P, nrow=2, ncol=2))
     stop = time.time()
     print("time = "+str(round(stop-start))+" sec")
 
