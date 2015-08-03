078ac546bc99960c95e2969e5af65d18ccca14fe
alex <alexandre.courtiol@gmail.com>
2015-08-03 15:25:52 +0200

    update tests

--- tests_for_likelihood.py
+++ tests_for_likelihood.py
@@ -12,19 +12,27 @@ def translate(Q, P):
 
 Q = np.array([[50, 4], [30, 2]], dtype=int)
 P = np.array([[1.0, 0.8], [0.5, 0.2]], dtype=float)
+# forward simu in R = 0.3322
+print(L1.likelihood(Q, P))
+print(L2.likelihood(*translate(Q, P)))
+
+Q = np.array([[50, 4, 2], [30, 2, 15]], dtype=int)
+P = np.array([[1.0, 0.8, 0.2], [0.5, 0.2, 0.7]], dtype=float)
+# forward simu in R = 0.0752
 print(L1.likelihood(Q, P))
 print(L2.likelihood(*translate(Q, P)))
 
 Q = np.array([[1, 1], [1, 1]], dtype=int)
 P = np.array([[0.0, 0.0], [0.0, 0.0]], dtype=float)
-
+# forward simu in R = impossible
 print(L1.likelihood(Q, P, limit_zero=False))
 print(L2.likelihood(*translate(Q, P), limit_zero=False))
-
-print(L1.likelihood(Q, P, limit_zero=True))
-print(L2.likelihood(*translate(Q, P), limit_zero=True))
+# forward simu in R = 0.66
+print(L1.likelihood(Q, P, limit_zero=True))  # BUG, wrong!
+print(L2.likelihood(*translate(Q, P), limit_zero=True))  # BUG, wrong!
 
 Q = np.array([[5, 4, 4], [4, 2, 1], [1, 1, 1]], dtype=int)
 P = np.array([[0.5, 0.6, 0.8], [0.7, 0.8, 0.9], [0.5, 0.4, 0.2]], dtype=float)
+# forward simu in R = 0.01768
 print(L1.likelihood(Q, P))
 print(L2.likelihood(*translate(Q, P)))
