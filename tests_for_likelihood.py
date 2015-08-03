4c280a9ed7d475b052a92aa40e60c74f015e76fc
alex <alexandre.courtiol@gmail.com>
2015-08-03 15:04:15 +0200

    new tests

--- tests_for_likelihood.py
+++ tests_for_likelihood.py
@@ -2,9 +2,6 @@ import numpy as np
 import likelihood_with_Fij as L1
 import likelihood_with_Fij_for_c_perspective as L2
 
-Q = np.array([[50, 4], [30, 2]], dtype=int)
-P = np.array([[1.0, 0.8], [0.5, 0.2]], dtype=float)
-
 def translate(Q, P):
     Pflat = list(P.flatten())
     Qflat = list(Q.flatten())
@@ -12,5 +9,18 @@ def translate(Q, P):
     ncol = np.shape(Q)[1]
     return Qflat, Pflat, nrow, ncol
 
+
+Q = np.array([[50, 4], [30, 2]], dtype=int)
+P = np.array([[1.0, 0.8], [0.5, 0.2]], dtype=float)
+print(L1.likelihood(Q, P))
+print(L2.likelihood(*translate(Q, P)))
+
+Q = np.array([[1, 1], [1, 1]], dtype=int)
+P = np.array([[0.0, 0.0], [0.0, 0.0]], dtype=float)
+print(L1.likelihood(Q, P, limit_zero=True))
+print(L2.likelihood(*translate(Q, P), limit_zero=True))
+
+Q = np.array([[20, 10, 10], [10, 20, 10], [1, 1, 1]], dtype=int)
+P = np.array([[0.5, 0.6, 0.8], [0.7, 0.8, 0.9], [0.5, 0.4, 0.2]], dtype=float)
 print(L1.likelihood(Q, P))
 print(L2.likelihood(*translate(Q, P)))
